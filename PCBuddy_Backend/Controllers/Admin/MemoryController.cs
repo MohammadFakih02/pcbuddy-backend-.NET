0e1fb70c7fcdcd9d3f38dc8a2832b24feced2254
MohammadFakih02 <mohammadfakih025@gmail.com>
2026-01-01 19:05:57 +0200

    added memory views and conroller

---
+++ PCBuddy_Backend/Controllers/Admin/MemoryController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PCBuddy_Backend.Data;
+using PCBuddy_Backend.Models;
+
+namespace PCBuddy_Backend.Controllers.Admin
+{
+    [Authorize(AuthenticationSchemes = "Cookies", Roles = "ADMIN")]
+    [Route("Admin/Memory")]
+    public class MemoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MemoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Memory.Where(m => !m.IsDeleted).ToListAsync());
+        }
+
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var memory = await _context.Memory.FirstOrDefaultAsync(m => m.Id == id);
+            if (memory == null) return NotFound();
+            return View(memory);
+        }
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Memory memory)
+        {
+            if (ModelState.IsValid)
+            {
+                memory.UpdatedAt = DateTime.UtcNow;
+                memory.IsDeleted = false;
+                _context.Add(memory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(memory);
+        }
+
+        [HttpGet("Edit/{id}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var memory = await _context.Memory.FindAsync(id);
+            if (memory == null) return NotFound();
+            return View(memory);
+        }
+
+        [HttpPost("Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Memory memory)
+        {
+            if (id != memory.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    memory.UpdatedAt = DateTime.UtcNow;
+                    _context.Update(memory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Memory.Any(e => e.Id == memory.Id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(memory);
+        }
+
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var memory = await _context.Memory.FirstOrDefaultAsync(m => m.Id == id);
+            if (memory == null) return NotFound();
+            return View(memory);
+        }
+
+        [HttpPost("Delete/{id}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var memory = await _context.Memory.FindAsync(id);
+            if (memory != null)
+            {
+                memory.IsDeleted = true;
+                memory.UpdatedAt = DateTime.UtcNow;
+                _context.Update(memory);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
